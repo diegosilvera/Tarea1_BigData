1abea2efb59c88513a671283945e0bd674ebbe31
LeidyRomero <lj.romero@uniandes.edu.co>
2020-08-21 15:10:19 -0500

    exportando resultados hasta punto d

--- words.py
+++ words.py
@@ -7,18 +7,23 @@ Created on Sun Aug 16 08:46:46 2020
 from bs4 import BeautifulSoup
 import urllib.request
 import tarfile as tf
-import re
+import re,os
 from collections import Counter
 from itertools import chain
 import numpy as np
-tfstream=urllib.request.urlopen('https://archive.ics.uci.edu/ml/machine-learning-databases/reuters21578-mld/reuters21578.tar.gz')
-reuters=tf.open(fileobj=tfstream,mode='r:gz')
+PATH="../Resultados/"
+if not os.path.exists(os.path.dirname(PATH)):
+    os.makedirs(os.path.dirname(PATH))
+
+#tfstream=urllib.request.urlopen('https://archive.ics.uci.edu/ml/machine-learning-databases/reuters21578-mld/reuters21578.tar.gz')
+#reuters=tf.open(fileobj=tfstream,mode='r:gz')
+reuters=tf.open("../reuters21578.tar.gz",mode='r:gz')
 reuters.extractall()
 reuters.close()
 
 #a. Cuenta el número de palabras en un archivo, dado el nombre del archivo. Por defecto, escogimos reut2-002.sgm
 # param id_file el nombre del archivo escogido.
-def how_many_words(id_file='reut2-002.sgm'):
+def how_many_words(id_file):
     soup=BeautifulSoup(open(id_file),'html.parser')
     ocurrences=soup.find_all('body')
     how_many=0
@@ -30,7 +35,7 @@ def how_many_words(id_file='reut2-002.sgm'):
 #b. Cuenta el número de veces que cierta palabra aparece en un archivo, con el nombre del archivo pasado por parámetro
 # param id_file el nombre del archivo escogido
 # param word La palabra para la cual se hace el conteo.
-def how_many_times(id_file='reut2-002.sgm',word):
+def how_many_times(id_file, word):
 
     soup=BeautifulSoup(open(id_file),'html.parser')
     ocurrences=soup.find_all('body',string=re.compile(word))
@@ -54,40 +59,88 @@ def words_in_news(id_file):
 #c y d. Retorna las N palabras más comúnes en un archivo. Por defecto se pasa reut-002.sgm
 # param id_file el nombre del archivo escogido
 # param N el número de palabras
-def N_most_common_words_in_news(id_file='reut2-002.sgm', N):
+def N_most_common_words_in_news(id_file, N):
     words=words_in_news(id_file)
     counter=Counter(chain.from_iterable(words))
     return counter.most_common(N)
 
-# e. Retorna una tupla de dos elementos donde cada uno de los elementos es una lista con las N palabras más repetidas en dos archivos.
-# param id_file_1 el nombre de uno de los dos archivos
-# param id_file_2 el nombre del segundo archivo
-# param N el número de palabras
-def N_most_common_words_in_news_two(id_file_1,id_file_2,N):
-    return N_most_common_words_in_news(id_file_1,N),N_most_common_words_in_news(id_file_2,N)
+#----------------------------------------------------------------------------------------------------------------------
+#Exportando respuesta reto a
+print("Procesando reto a...")
+how_many_w=how_many_words('reut2-002.sgm')
+print("Exportando resultados del reto a")
+fileRetoA = open(PATH+"resultados_reto_A.txt","w")
+fileRetoA.write("El archivo reut2-002.sgm tiene "+str(how_many_w)+" palabras")
+fileRetoA.close()
 
-# f. Retorna una lista con una cierta cantidad de elementos donde cada elemento es una lista que está en biyección con cada archivo en una lista X de archivos
-# param X lista con nombres de archivos
-# param N el número de palabras
-def N_most_common_words_in_news_X(X, N):
-    N_most_common=[]
-    for id_file in X:
-        N_most_common.append(N_most_common_words_in_news(id_file,N))
-    return N_most_common
+#Exportando respuesta reto b
+def reto_b(id_file):
+    words=words_in_news(id_file)
+    times=[]
+    for wordList in words:
+        for word in wordList:
+            how_many_t=how_many_times(id_file,word)
+            times.append(word+":"+str(how_many_t))
+    return times
 
-# g. Retorna una tupla de dos elementos, donde el primero de estos corresponde al nombre del archivo en una lista de X archivos en el que aparece más veces cierta palabra,
-#    y el segundo elemento dice cuál archivo tiene más palabras.
-# param X lista con nombres de archivos
-# param word la palabra escogida
-def which_file_has_more_ocurrences(X,word):
-    words_ocurrences=[]
-    lengths=[]
-    for id_file in X:
-        length=how_many_words(id_file)
-        times=how_many_times(id_file,word)
-        lengths.append(length)
-        words_ocurrences.append(times)
-        
-    words_ocurrences=np.array(words_ocurrences)
-    lengths=np.array(lengths)
-    return X[np.argmax(words_ocurrences)],X[np.argmax(lengths)]
+print("Procesando reto b...")
+#times=reto_b('reut2-002.sgm')
+print("Exportando resultados del reto b")
+fileRetoB = open(PATH+"resultados_reto_B.txt","w")
+#for frase in times:
+    #fileRetoB.write(frase)
+fileRetoB.close()
+
+#Exportando respuesta reto c
+print("Digite el numero de palabras:")
+N=input()
+print("Procesando reto c...")
+words=N_most_common_words_in_news('reut2-002.sgm',int(N))
+print("Exportando resultados del reto c")
+fileRetoC = open(PATH+"resultados_reto_C.txt","w")
+for pair in words:
+    fileRetoC.write(pair[0])
+fileRetoC.close()
+
+#Exportando respuesta reto d
+print("Digite el numero de palabras:")
+N=input()
+print("Digite nombre del archivo:")
+id_file=input()
+print("Procesando reto d...")
+words=N_most_common_words_in_news(id_file,int(N))
+print("Exportando resultados del reto d")
+fileRetoD = open(PATH+"resultados_reto_D.txt","w")
+for w in words:
+    fileRetoD.write(w[0])
+fileRetoD.close()
+
+#---------------------------------------------E:EXPORTANDO ARCHIVOS DE RETOS A-D CON 2 ARCHIVOS---------------------------- 
+#Exportando respuesta reto a
+print("Procesando reto E-A...")
+how_many_w=how_many_words('reut2-002.sgm')+how_many_words('reut2-013.sgm')
+print("Exportando resultados del reto E-A")
+
+#Exportando respuesta reto b
+print("Procesando reto E-B...")
+#times=reto_b('reut2-002.sgm')+reto_b('reut2-013.sgm')
+print("Exportando resultados del reto E-B")
+
+#Exportando respuesta reto c
+print("Digite el numero de palabras:")
+N=input()
+print("Procesando reto E-C...")
+words=N_most_common_words_in_news('reut2-002.sgm',int(N))+N_most_common_words_in_news('reut2-013.sgm',int(N))
+print("Exportando resultados del reto E-C")
+
+#Exportando respuesta reto d
+print("Digite el numero de palabras:")
+N=input()
+print("Digite nombre del primer archivo:")
+id_file=input()
+print("Digite nombre del segundo archivo:")
+id_file2=input()
+print("Procesando reto E-D...")
+words=N_most_common_words_in_news(id_file,N)+N_most_common_words_in_news(id_file2,N)
+print("Exportando resultados del reto E-D")
+#---------------------------------------------F:EXPORTANDO ARCHIVOS DE RETOS A-D CON X ARCHIVOS---------------------------- 
